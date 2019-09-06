8d91dde08f87cd079ea14a71df9eae5b29f62251
Aditya Bhatia <bhatia.aditya@gmail.com>
2019-09-05 23:12:16 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/* This is a simple Java program.     FileName : "HelloWorld.java". */ 
+class HelloWorld {
+   // Your program begins with a call to main().
+   // Prints "Hello, World" to the terminal window. 
+   public static void main(String args[]){    
+      int[] arrayTest = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+      System.out.println("Hello, World");
+      System.out.println("Goodbye");
+      for(int i = 0; i <= arrayTest.length - 1; i++){
+         System.out.println(arrayTest[i]);
+      }
+      System.out.println("");
+      for(int i = 0; i <= arrayTest.length - 1; i++){
+         System.out.println(arrayTest[i]);
+      }
+   }
+}
